16d8aa85876ec9e0b7c3398ce0e542a67f79c62c
marcBiason <54872072+marcioBiason@users.noreply.github.com>
2019-10-07 18:01:49 -0300

    Add files via upload

---
+++ ExercicioInterfaceAluguelAutomovel/ExercicioInterfaceAluguelAutomovel/Entities/Vehicle.cs
@@ -0,0 +1,12 @@
+﻿namespace ExercicioInterfaceAluguelAutomovel.Entities
+{
+    class Vehicle
+    {
+        public string Model { get; set; }
+
+        public Vehicle(string model)
+        {
+            Model = model;
+        }
+    }
+}
